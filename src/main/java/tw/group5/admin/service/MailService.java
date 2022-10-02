1bf189f6f56484c0d418f8f828f4338bd65c7324
kelly2643 <r08625027@ntu.edu.tw>
2022-10-02 13:22:37 +0800

    email功能正式上線，有模板功能~~欲使用者請洽技術長~~我周一到三不在~~應該三晚回來~~

---
+++ src/main/java/tw/group5/admin/service/MailService.java
@@ -0,0 +1,34 @@
+package tw.group5.admin.service;
+
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+    
+    public void mimeMail(String fromEmail, List<String> toList, String subject, String html) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom(fromEmail);
+            for (String to : toList) {
+                messageHelper.addTo(to);
+            }
+            messageHelper.setSubject(subject);
+            messageHelper.setText(html, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
